39f24ad8fde703e2e5ac1ea8f669bd1993636021
Sam Goldenberg <sgolden140@gmail.com>
2022-05-04 19:12:01 -0700

    Created index.html, javascript.js and style.css for basic-website

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// javascript for basic-website
+console.log("hello world");
+
+let funAdjectives = ["great", "super", "fantastic"];
+
+for (let i = 0; i < funAdjectives.length; i++) {
+    console.log(i);
+    console.log(funAdjectives[i]);
+}
+
+
