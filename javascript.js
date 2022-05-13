1470670f332c04bfd78d4cf821c6ffbc31b22823
Sam Goldenberg <sgolden140@gmail.com>
2022-05-13 13:54:14 -0700

    Created about.html, linked to other pages

--- javascript.js
+++ javascript.js
@@ -8,4 +8,23 @@ for (let i = 0; i < funAdjectives.length; i++) {
     console.log(funAdjectives[i]);
 }
 
+let objectPractice = {
+    name: "Sam",
+    age: 31,
+    state: "Oregon",
+}
 
+for (let key in objectPractice) {
+    console.log(key + " " + objectPractice[key]);
+}
+
+let greeting = `Hello, ${objectPractice.name}`;
+console.log(greeting);
+
+// let name = prompt("What is your name?");
+
+let welcomeMessage = `Welcome to basic-website, ${name}! Let me know if you have any comments or questions.`;
+
+console.log(welcomeMessage)
+
+// alert(welcomeMessage);
